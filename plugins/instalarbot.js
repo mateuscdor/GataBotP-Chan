3814796bc3b4d7e4122120dc0473478ff3936ab0
PECHAN04 <103233109+PECHAN04@users.noreply.github.com>
2022-05-01 14:57:30 -0500

    Update instalarbot.js

--- plugins/instalarbot.js
+++ plugins/instalarbot.js
@@ -5,40 +5,6 @@ let handler = async m => m.reply(`
 
 
 
-✨ 𝙍𝙀𝙌𝙐𝙄𝙎𝙄𝙏𝙊𝙎 𝙋𝘼𝙍𝘼 𝙄𝙉𝙎𝙏𝘼𝙇𝘼𝙍 𝙂𝘼𝙏𝘼𝘽𝙊𝙏
-
-✅ _1 GB de almacenamiento_
-✅ _Aplicación Termux (actualizada)_
-✅ _Un WhatsApp inmune (secundario)_
-✅ _Un número virtual_
-✅ _2 dispositivos o una PC para escanear_
-
-
-🌸 𝙂𝙄𝙏𝙃𝙐𝘽 > 𝙍𝙀𝙋𝙊𝙎𝙄𝙏𝙊𝙍𝙄𝙊
-*_Visita mí repositorio 😸 para más información, si te agrada el Bot apoya me con una ⭐️ ¡Gracias!_*
-
-
-
-
-❇️ 𝘾𝙊𝙈𝘼𝙉𝘿𝙊𝙎 𝘿𝙀 𝙄𝙉𝙎𝙏𝘼𝙇𝘼𝘾𝙄𝙊́𝙉 𝙑𝙄́𝘼 𝙂𝙄𝙏𝙃𝙐𝘽 
-
-> 
-*_PARA TENER UNA GUIA DE COMO INSTALAR A GATABOT_*
-*_USA EL COMANDO;_*
-#procesobot
-
-💡 *_NOTA:_*
-*_Lea la información del archivo README.md de GataBot:_*
-
-*_https://github.com/GataNina-Li/GataBotV2/blob/master/README.md_*
-
-⚠️ *_Si usas un número importante para que sea Bot, o un WhatsApp normal es posible que el número pueda irse al soporte_*
-
-💛 *_Si tienes dudas o necesitas ayuda en el proceso de la instalación puede escribir me a este número (No es Bot el número)_*
-
-📲 *_Wa.me/14509544207_*
-
-❗ *_Solo para temas de instalación_* 
 
 ⚡▁ ▂ ▄ ▅ ▆ ▇ █ 🚆 █ ▇ ▆ ▅ ▄ ▂ ▁⚡
                             🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈
